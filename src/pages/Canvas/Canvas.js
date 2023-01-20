485c76cc11418fe2b50d8f66e27f8201e656392e
moris96 <moriskhoudari@gmail.com>
2023-01-20 15:39:33 -0500

    w key moves character but only down for now. got the w key to work

--- src/pages/Canvas/Canvas.js
+++ src/pages/Canvas/Canvas.js
@@ -3,28 +3,106 @@ export default function Canvas() {
     const ctx = canvas.getContext('2d')
 
     //draw canvas
-    function drawCanvas() {
-        canvas.width = 1024
-        canvas.height = 576
-        ctx.fillStyle = 'white'
-        ctx.fillRect(0,0, canvas.width, canvas.height)
-    }
-    drawCanvas()
-    
+    canvas.width = 1024
+    canvas.height = 576
+    ctx.fillStyle = 'white'
+    ctx.fillRect(0,0, canvas.width, canvas.height)
+
     //place images on canvas 
-    function drawImage() {
-        //background image
-        const image = new Image()
-        image.src = './pokemonMap/MapZoom.png'
-        //playerDown image 
-        const playerImage = new Image()
-        playerImage.src = './player/playerDown.png'
+    //background image
+    const image = new Image()
+    image.src = './pokemonMap/MapZoom.png'
+    //playerDown image 
+    const playerImage = new Image()
+    playerImage.src = './player/playerDown.png'
         
-        image.onload = () => {
-            ctx.drawImage(image, -1200, -70)
-            ctx.drawImage(playerImage, 0, 0, playerImage.width/4, playerImage.height, canvas.width/2 - (playerImage.width/4)/4, canvas.height/2 - playerImage.height/2, playerImage.width/4, playerImage.height)
+    // image.onload = () => {
+    //     ctx.drawImage(image, -1200, -70)
+    //     ctx.drawImage(playerImage, 0, 0, playerImage.width/4, playerImage.height, canvas.width/2 - (playerImage.width/4)/4, canvas.height/2 - playerImage.height/2, playerImage.width/4, playerImage.height)
+    // }
+
+    class Sprite {
+        constructor({ position, velocity, image }) {
+            this.position = position
+            this.image = image
+        }
+
+        draw() {
+            ctx.drawImage(this.image, this.position.x, this.position.y)
         }
     }
-    drawImage()
+
+    const background = new Sprite({
+        position: {
+            x: -1200,
+            y: -70
+        },
+        image: image
+    })
+
+    const keys = {
+        w: {
+            pressed: false 
+        },
+        a: {
+            pressed: false 
+        },
+        s: {
+            pressed: false 
+        },
+        d: {
+            pressed: false 
+        }
+    }
+
+    //animate player sprite
+    function animate() {
+        window.requestAnimationFrame(animate)
+        background.draw()
+        // ctx.drawImage(image, -1200, -70)
+        ctx.drawImage(playerImage, 0, 0, playerImage.width/4, playerImage.height, canvas.width/2 - (playerImage.width/4)/4, canvas.height/2 - playerImage.height/2, playerImage.width/4, playerImage.height)
+
+        if(keys.w.pressed) {
+            background.position.y = background.position.y - 3
+        }
+        
+    }
+    animate()
+
+    //move player through map on keydown
+    window.addEventListener('keydown', (e) => {
+        switch (e.key) {
+            case 'w':
+                keys.w.pressed = true 
+                break 
+            case 'a':
+                keys.a.pressed = true 
+                break 
+            case 's':
+                keys.s.pressed = true 
+                break 
+            case 'd':
+                keys.d.pressed = true 
+                break 
+        }
+    })
+    //keyup
+    window.addEventListener('keyup', (e) => {
+        switch (e.key) {
+            case 'w':
+                keys.w.pressed = false
+                break 
+            case 'a':
+                keys.a.pressed = false 
+                break 
+            case 's':
+                keys.s.pressed = false 
+                break 
+            case 'd':
+                keys.d.pressed = false 
+                break 
+        }
+    })
+
 
 };
