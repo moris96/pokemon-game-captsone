18cf599be5f2f19c77ce24e33517cf9bf153a2f4
moris96 <moriskhoudari@gmail.com>
2023-02-01 14:49:32 -0500

    centred elements in attack bar

--- src/pages/Canvas/Canvas.js
+++ src/pages/Canvas/Canvas.js
@@ -473,7 +473,16 @@ export default function Canvas() {
 
     return (
         <>   
-            {/* <div className="attack-bar" /> */}
+
+            {/* <div id="attack-bar">
+                <div class="attack-button">
+                    <button>Attack 1</button>
+                </div>
+            </div> */}
+
+            {/* <div className="attack-button">
+                <button>Attack 1</button>
+            </div> */}
 
             <div className="display-block">
                 <div className="overlapping-div" />
