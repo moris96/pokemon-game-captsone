b3d4c3bbb785e7f9fbe3ebdc6b316e5db9cfe653
moris96 <moriskhoudari@gmail.com>
2023-02-02 16:35:22 -0500

    added functionality for physical attacks

--- src/pages/Canvas/Canvas.js
+++ src/pages/Canvas/Canvas.js
@@ -96,7 +96,7 @@ export default function Canvas() {
 
 
     class Sprite {
-        constructor({ position, velocity, image, frames = { max: 1, hold: 10 }, sprites, animate = false }) {
+        constructor({ position, velocity, image, frames = { max: 1, hold: 10 }, sprites, animate = false, isEnemy = false }) {
             this.position = position
             this.image = image
             this.frames = {...frames, val: 0, elapsed: 0}
@@ -108,6 +108,8 @@ export default function Canvas() {
             this.animate = animate 
             this.sprites = sprites
             this.opacity = 1 
+            this.health = 100 
+            this.isEnemy = isEnemy
         }
         draw() {
             ctx.save()
@@ -139,13 +141,24 @@ export default function Canvas() {
         attack({ attack, recipient }) {
             const tl = gsap.timeline()
 
+            this.health = this.health - attack.damage
+
+            let movementDistance = this.isEnemy ? -20 : 20 
+
+            let healthBear = this.isEnemy ? '.charizard-health2' : '.elon-health2'
+
             tl.to(this.position, {
-                x: this.position.x - 20
+                x: this.position.x - movementDistance
             })
             .to(this.position, {
-                x: this.position.x + 40,
+                x: this.position.x + movementDistance * 2,
                 duration: .1,
-                onComplete() {
+                onComplete: () => {
+                    //enemy gets hit 
+                    gsap.to(healthBear, {
+                        width: `${this.health}%`
+                    })
+
                     gsap.to(recipient.position, {
                         x: recipient.position.x + 10,
                         yoyo: true,
@@ -428,7 +441,8 @@ export default function Canvas() {
             max: 4,
             hold: 30
         },
-        animate: true 
+        animate: true,
+        isEnemy: true 
     }) 
 
     //charizard (hero) image
@@ -462,9 +476,9 @@ export default function Canvas() {
         button.addEventListener('click', () => {
             charizard.attack({ 
                 attack: {
-                    name: 'Flamethrower',
+                    name: 'Slash',
                     damage: 10,
-                    type: 'Fire'
+                    type: 'Normal'
             },
             recipient: elon 
             })
